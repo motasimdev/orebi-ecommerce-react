5e755eef2c3bfb0dca70434cfcb53b2ef2a2996e
barakahtrust <barakahtrustbd@gmail.com>
2025-07-04 22:21:33 +0600

    img

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+    <div className="bg-teal-400 py-5 text-center">Footer</div>
+    </>
   )
 }
 
--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Container from '../Container'
+import Flex from '../Flex'
+import Image from '../Image'
+import logo from '/src/assets//logo.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <>
+    <div className="bg-amber-400 py-[30px]">
+      <Container>
+        <Flex>
+          <div className="">
+            <Image src={logo} alt={"logo"}/>
+          </div>
+          <div className=""></div>
+          <div className=""></div>
+        </Flex>
+      </Container>
+    </div>
+    </>
   )
 }
 
