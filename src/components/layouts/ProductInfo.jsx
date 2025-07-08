8d260ba94157a6451a0473b32770d708879f9649
barakahtrust <barakahtrustbd@gmail.com>
2025-07-08 11:47:32 +0600

    product

--- src/components/layouts/ProductInfo.jsx
+++ src/components/layouts/ProductInfo.jsx
@@ -12,59 +12,79 @@ const ProductInfo = () => {
           <Heading
             text={"Product"}
             as={"h3"}
-            className={"text-[#262626] text-[39px] font-bold"}
+            className={"text-[#262626] text-[39px] font-bold pb-6"}
           />
           {/* =============== */}
           <Flex className={"gap-x-4"}>
             <div className="flex items-center">
-              <FaStar className="text-amber-400" />
-              <FaStar className="text-amber-400" />
-              <FaStar className="text-amber-400" />
-              <FaStar className="text-amber-400" />
-              <FaStar className="text-amber-400" />
+              <FaStar className="text-[#FFD881] text-sm" />
+              <FaStar className="text-[#FFD881] text-sm" />
+              <FaStar className="text-[#FFD881] text-sm" />
+              <FaStar className="text-[#FFD881] text-sm" />
+              <FaStar className="text-[#FFD881] text-sm" />
             </div>
             <div className="">
-              <Heading text={"1 review"} as={"p"} className={""} />
+              <Heading
+                text={"1 review"}
+                as={"p"}
+                className={"text-sm text-[#767676]"}
+              />
             </div>
           </Flex>
 
           {/* ==================== */}
           <Flex className={"gap-x-5"}>
-            <div className="">
-              <Heading text={"$ 88.00"} as={"p"} className={"line-through"} />
+            <div className="py-7">
+              <Heading
+                text={"$ 88.00"}
+                as={"p"}
+                className={"line-through text-[#767676]"}
+              />
             </div>
             <div className="">
-              <Heading text={"$ 44.00"} as={"p"} className={""} />
+              <Heading
+                text={"$ 44.00"}
+                as={"p"}
+                className={"text-[#262626] text-2xl font-bold"}
+              />
             </div>
           </Flex>
           <div className="w-[780px] h-[1px] bg-[#F0F0F0]"></div>
 
           {/* ========================= */}
-          <Flex>
-            <div className="">
-              <Heading text={"Color:"} as={"p"} className={""} />
-            </div>
-            <div className="flex items-center">
-              <div className="w-5 h-5 rounded-full bg-[#979797]"></div>
-              <div className="w-7 h-7 rounded-full bg-[#FF8686]"></div>
-              <div className="w-5 h-5 rounded-full bg-[#7ED321]"></div>
-              <div className="w-5 h-5 rounded-full bg-[#B6B6B6]"></div>
-              <div className="w-5 h-5 rounded-full bg-[#15CBA5]"></div>
-            </div>
-          </Flex>
+          <div className="py-7">
+            <Flex>
+              <div className="">
+                <Heading
+                  text={"Color:"}
+                  as={"p"}
+                  className={"pr-[50px] uppercase text-base font-bold text-[#262626]"}
+                />
+              </div>
+              <div className="flex items-center">
+                <div className="w-5 h-5 rounded-full bg-[#979797] mr-[12px]"></div>
+                <div className="w-7 h-7 rounded-full bg-[#FF8686] mr-[12px]"></div>
+                <div className="w-5 h-5 rounded-full bg-[#7ED321] mr-[12px]"></div>
+                <div className="w-5 h-5 rounded-full bg-[#B6B6B6] mr-[12px]"></div>
+                <div className="w-5 h-5 rounded-full bg-[#15CBA5]"></div>
+              </div>
+            </Flex>
+          </div>
 
           {/* ======================== */}
-          <Flex>
-            <Heading text={"Size:"} as={"p"} className={""} />
-            <div className="">
-              <select defaultValue="S" className="select pr-[70px]">
-                <option disabled={true}>S</option>
-                <option>L</option>
-                <option>Xl</option>
-                <option>XXl</option>
-              </select>
-            </div>
-          </Flex>
+          <div className="">
+            <Flex>
+              <Heading text={"Size:"} as={"p"} className={"uppercase pr-[70px] text-base font-bold text-[#262626]"} />
+              <div className="">
+                <select defaultValue="S" className="select pr-[70px]">
+                  <option disabled={false}>S</option>
+                  <option>L</option>
+                  <option>XL</option>
+                  <option>XXL</option>
+                </select>
+              </div>
+            </Flex>
+          </div>
         </Container>
       </div>
     </>
