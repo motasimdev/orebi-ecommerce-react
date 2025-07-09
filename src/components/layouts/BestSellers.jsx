a5d4f6a7763b4a6b5525d5d5815a9db2a8230b2e
barakahtrust <barakahtrustbd@gmail.com>
2025-07-09 16:57:45 +0600

    best seler

--- src/components/layouts/BestSellers.jsx
+++ src/components/layouts/BestSellers.jsx
@@ -1,8 +1,52 @@
 import React from 'react'
+import SingleProduct from "../SingleProduct";
+import Container from "../Container";
+import Flex from "../Flex";
+import watchA from "/src/assets/watchA.png";
+import watchB from "/src/assets/watchB.png";
+import jhuri from "/src/assets/jhuri.png";
+import putul from "/src/assets/putul.png";
+import Heading from "../Heading";
 
 const BestSellers = () => {
   return (
-    
+    <>
+      <div className="pb-6">
+        <Container>
+            <Heading text={"New Arrival"} as={"h3"} className={"font-bold text-[39px] pb-15 pt-12"}/>
+          <Flex className={"gap-x-5"}>
+            <SingleProduct
+              BadgeText={"10%"}
+              productTitle={"Basic Crew Neck Tee"}
+              productPrice={"$44.00"}
+              src={watchA}
+              alt={"watchA"}
+            />
+            <SingleProduct
+              BadgeText={"10%"}
+              productTitle={"Basic Crew Neck Tee"}
+              productPrice={"$44.00"}
+              src={watchB}
+              alt={"watchB"}
+            />
+            <SingleProduct
+              BadgeText={"10%"}
+              productTitle={"Basic Crew Neck Tee"}
+              productPrice={"$44.00"}
+              src={jhuri}
+              alt={"jhuri"}
+            />
+            <SingleProduct
+              BadgeText={"10%"}
+              productTitle={"Basic Crew Neck Tee"}
+              productPrice={"$44.00"}
+              src={putul}
+              alt={"putul"}
+            />
+          </Flex>
+        </Container>
+      </div>
+    </>
   )
 }
 
