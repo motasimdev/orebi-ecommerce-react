73523d6766fcfbe4914c2b90e21a04b081145c29
barakahtrust <barakahtrustbd@gmail.com>
2025-07-09 16:56:23 +0600

    new arrival

--- src/components/SingleProduct.jsx
+++ src/components/SingleProduct.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Badge from "./Badge";
 import Image from "./Image";
-import watchA from "/src/assets/watchA.png";
 import { Link } from "react-router";
 import Heading from "./Heading";
 import { FaHeart, FaShoppingCart } from "react-icons/fa";
@@ -16,7 +15,7 @@ const SingleProduct = ({BadgeText, src, alt, productTitle, productPrice }) => {
         <Badge className={"absolute top-4 left-4"} text={BadgeText} />
 
         {/* ================ */}
-        <div className="bg-white p-7.5 absolute bottom-9 left-0 w-full translate-y-4 opacity-0 group-hover:opacity-100 hover:translate-y-0 transition-all ease-in-out duration-300">
+        <div className="bg-white p-7.5 absolute bottom-9 left-0 w-full translate-y-4 opacity-0 group-hover:translate-y-0 group-hover:opacity-100 transition-all duration-300 ease-in-out">
           <Link>
             <Flex className={"justify-end gap-x-3 "}>
               <Heading text={"Add to Wish List"} as={"p"} className={"text-[#767676] text-base"} />
--- src/components/layouts/NewArrival.jsx
+++ src/components/layouts/NewArrival.jsx
@@ -1,27 +1,53 @@
-import React from 'react'
-import SingleProduct from '../SingleProduct'
-import Container from '../Container'
-import Flex from '../Flex'
-import watchA from '/src/assets/watchA.png'
-import watchB from '/src/assets/watchB.png'
-import jhuri from '/src/assets/jhuri.png'
-import putul from '/src/assets/putul.png'
+import React from "react";
+import SingleProduct from "../SingleProduct";
+import Container from "../Container";
+import Flex from "../Flex";
+import watchA from "/src/assets/watchA.png";
+import watchB from "/src/assets/watchB.png";
+import jhuri from "/src/assets/jhuri.png";
+import putul from "/src/assets/putul.png";
+import Heading from "../Heading";
 
 const NewArrival = () => {
   return (
     <>
-    <div className="pb-6">
+      <div className="pb-6">
         <Container>
-            <Flex className={"gap-x-5"}>
-                <SingleProduct BadgeText={"10%"} src={watchA} alt={"watchA"}/>
-                <SingleProduct BadgeText={"10%"} src={watchB} alt={"watchB"}/>
-                <SingleProduct BadgeText={"10%"} src={jhuri} alt={"jhuri"}/>
-                <SingleProduct BadgeText={"10%"} src={putul} alt={"putul"}/>
-            </Flex>
+            <Heading text={"New Arrival"} as={"h3"} className={"font-bold text-[39px] pb-15 pt-12"}/>
+          <Flex className={"gap-x-5"}>
+            <SingleProduct
+              BadgeText={"10%"}
+              productTitle={"Basic Crew Neck Tee"}
+              productPrice={"$44.00"}
+              src={watchA}
+              alt={"watchA"}
+            />
+            <SingleProduct
+              BadgeText={"10%"}
+              productTitle={"Basic Crew Neck Tee"}
+              productPrice={"$44.00"}
+              src={watchB}
+              alt={"watchB"}
+            />
+            <SingleProduct
+              BadgeText={"10%"}
+              productTitle={"Basic Crew Neck Tee"}
+              productPrice={"$44.00"}
+              src={jhuri}
+              alt={"jhuri"}
+            />
+            <SingleProduct
+              BadgeText={"10%"}
+              productTitle={"Basic Crew Neck Tee"}
+              productPrice={"$44.00"}
+              src={putul}
+              alt={"putul"}
+            />
+          </Flex>
         </Container>
-    </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default NewArrival
+export default NewArrival;
