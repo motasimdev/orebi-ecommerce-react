5774aa219dd56c718912bd20d15fa27fe72fa91d
barakahtrust <barakahtrustbd@gmail.com>
2025-07-11 23:58:44 +0600

    banner slider added

--- src/components/layouts/Banner.jsx
+++ src/components/layouts/Banner.jsx
@@ -1,16 +1,45 @@
-import React from 'react'
-import { Link } from 'react-router'
+import React from "react";
+import { Link } from "react-router";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import NextArrw from "../NextArrw";
+import PrevArrw from "../PrevArrw";
 
 const Banner = () => {
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    nextArrow: <NextArrw />,
+    prevArrow: <PrevArrw />,
+  };
+
   return (
     <>
-    <Link to={"/"}>
-        <div className="bg-[url(/src/assets/banner.png)] h-[600px] bg-no-repeat bg-center bg-cover">
-        
+      <Slider {...settings}>
+        <div>
+          <div className="">
+            <Link to={"/"}>
+              <div className="bg-[url(/src/assets/banner.png)] h-[600px] bg-no-repeat bg-center bg-cover"></div>
+            </Link>
+          </div>
         </div>
-    </Link>
+        <div>
+          <div className="">
+            <Link to={"/"}>
+              <div className="bg-[url(/src/assets/banner.png)] h-[600px] bg-no-repeat bg-center bg-cover"></div>
+            </Link>
+          </div>
+        </div>
+      </Slider>
     </>
-  )
-}
+  );
+};
 
-export default Banner
+export default Banner;
