5f8a5692a222bd3b790979d6a8a421fc23520790
motasimdev <themotasimdev@gmail.com>
2025-08-29 22:53:01 +0600

    menu responsive done

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -9,10 +9,8 @@ import { FaSearch, FaUser, FaAngleDown, FaShoppingCart } from "react-icons/fa";
 import { Link } from "react-router";
 import { HiBars3CenterLeft } from "react-icons/hi2";
 
-
 const Header = () => {
-
-  const [menu, setMenu] = useState(false)
+  const [menu, setMenu] = useState(false);
   return (
     <>
       <div className="py-[30px] hidden lg:block">
@@ -57,54 +55,53 @@ const Header = () => {
       </div>
 
       {/* responsive menu  */}
-      
-        <div className="py-[30px] lg:hidden">
-          <Container>
-            <div className="">
-              {/* <div className="">
-              <Link to={"/"}>
-                <Image src={logo} alt={"logo"} />
-              </Link>
-            </div> */}
+
+      <div className="py-[6px] lg:hidden">
+        <Container>
+          <div className="">
+            <div className="flex justify-between ">
               <button className="cursor-pointer" onClick={() => setMenu(!menu)}>
                 <HiBars3CenterLeft />
               </button>
-              {menu && (
-                <div className="">
-                  <ul className="bg-amber-200 w-[200px] text-center p-5">
-                    <Link to={"/"}>
-                      <li className="hover:text-[#262626] text-[#767676] text-sm pb-3">
-                        Home
-                      </li>
-                    </Link>
-                    <Link to={"/shop"}>
-                      <li className="hover:text-[#262626] text-[#767676] text-sm pb-3">
-                        Shop
-                      </li>
-                    </Link>
-                    <Link to={"/About"}>
-                      <li className="hover:text-[#262626] text-[#767676] text-sm pb-3">
-                        About
-                      </li>
-                    </Link>
-                    <Link to={"/Contact"}>
-                      <li className="hover:text-[#262626] text-[#767676] text-sm pb-3">
-                        Contact
-                      </li>
-                    </Link>
-                    <Link to={"/Journal"}>
-                      <li className="hover:text-[#262626] text-[#767676] text-sm">
-                        Journal
-                      </li>
-                    </Link>
-                  </ul>
-                </div>
-              )}
+              <Link to={"/"}>
+                <Image src={logo} alt={"logo"} className={""}/>
+              </Link>
             </div>
-          </Container>
-        </div>
+            {menu && (
+              <div className="">
+                <ul className="bg-amber-200 w-[200px] text-center p-5">
+                  <Link to={"/"}>
+                    <li className="hover:text-[#262626] text-[#767676] text-sm pb-3">
+                      Home
+                    </li>
+                  </Link>
+                  <Link to={"/shop"}>
+                    <li className="hover:text-[#262626] text-[#767676] text-sm pb-3">
+                      Shop
+                    </li>
+                  </Link>
+                  <Link to={"/About"}>
+                    <li className="hover:text-[#262626] text-[#767676] text-sm pb-3">
+                      About
+                    </li>
+                  </Link>
+                  <Link to={"/Contact"}>
+                    <li className="hover:text-[#262626] text-[#767676] text-sm pb-3">
+                      Contact
+                    </li>
+                  </Link>
+                  <Link to={"/Journal"}>
+                    <li className="hover:text-[#262626] text-[#767676] text-sm">
+                      Journal
+                    </li>
+                  </Link>
+                </ul>
+              </div>
+            )}
+          </div>
+        </Container>
+      </div>
       {/* responsive menu end */}
-      
 
       <div className="bg-[#F5F5F3] py-6">
         <Container>
