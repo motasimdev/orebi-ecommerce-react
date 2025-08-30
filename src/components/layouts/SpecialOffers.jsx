a2c93e8976dddfde2ce3b92cdd85259733266851
motasimdev <themotasimdev@gmail.com>
2025-08-30 14:07:14 +0600

    specialOffers responsive running

--- src/components/layouts/SpecialOffers.jsx
+++ src/components/layouts/SpecialOffers.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 import SingleProduct from "../SingleProduct";
 import Container from "../Container";
 import Flex from "../Flex";
@@ -12,12 +12,10 @@ import "slick-carousel/slick/slick-theme.css";
 import Slider from "react-slick";
 import NextArrw from "../NextArrw";
 import PrevArrw from "../PrevArrw";
-import axios from 'axios';
-import { useState, useEffect } from 'react';
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const SpecialOffers = () => {
-
-    
   var settings = {
     dots: false,
     infinite: true,
@@ -28,7 +26,20 @@ const SpecialOffers = () => {
     autoplay: true,
     autoplaySpeed: 1000,
     nextArrow: <NextArrw />,
-    prevArrow: <PrevArrw />
+    prevArrow: <PrevArrw />,
+  };
+
+  var mobileSettings = {
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: <NextArrw />,
+    prevArrow: <PrevArrw />,
   };
 
   const [myProduct, setMyproduct] = useState([]);
@@ -41,10 +52,9 @@ const SpecialOffers = () => {
     all();
   }, []);
 
-
   return (
     <>
-    <div className="pb-[110px]">
+      <div className="pb-[110px] hidden lg:block">
         <Container>
           <Heading
             text={"Special Offers"}
@@ -53,23 +63,50 @@ const SpecialOffers = () => {
           />
           <div className="-mx-2">
             <Slider {...settings}>
-            {myProduct.map((item) => (
-              <div key={item.id} className="px-4">
-                <SingleProduct
-                  BadgeText={"10%"}
-                  productTitle={item.title}
-                  productPrice={`$${item.price}`}
-                  imgSrc={item.thumbnail}
-                  alt={"watchA"}
-                />
-              </div>
-            ))}
-          </Slider>
+              {myProduct.map((item) => (
+                <div key={item.id} className="px-4">
+                  <SingleProduct
+                    BadgeText={"10%"}
+                    productTitle={item.title}
+                    productPrice={`$${item.price}`}
+                    imgSrc={item.thumbnail}
+                    alt={"watchA"}
+                  />
+                </div>
+              ))}
+            </Slider>
           </div>
         </Container>
       </div>
-    </>
-  )
-}
 
-export default SpecialOffers
+      {/* responsive------------ */}
+      <div className="lg:hidden">
+        <Container>
+          <Heading
+            text={"Special Offers"}
+            as={"h3"}
+            className={"font-bold text-[25px] py-3 text-center"}
+          />
+          <div className="bg-gray-100 h-[350px]">
+            <Slider {...mobileSettings}>
+              {myProduct.map((item) => (
+                <div key={item.id} className="px-4">
+                  <SingleProduct
+                    BadgeText={"10%"}
+                    productTitle={item.title}
+                    productPrice={`$${item.price}`}
+                    imgSrc={item.thumbnail}
+                    alt={"watchA"}
+                  />
+                </div>
+              ))}
+            </Slider>
+          </div>
+        </Container>
+      </div>
+      {/* responsive------------end */}
+    </>
+  );
+};
+
+export default SpecialOffers;
