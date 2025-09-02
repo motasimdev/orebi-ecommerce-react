44b9e3f528461574857a4633d136b504d832e4f5
motasimdev <themotasimdev@gmail.com>
2025-09-02 07:00:01 +0600

    menu adding

--- src/components/Catagories.jsx
+++ src/components/Catagories.jsx
@@ -5,6 +5,8 @@ import { FaArrowRightLong, FaArrowDownLong } from "react-icons/fa6";
 const Catagories = () => {
   const [menu, setMenu] = useState(false);
   const [subMenu, setSubmenu] = useState(false);
+  const [menu2, setMenu2] = useState(false);
+  const [subMenu2, setSubmenu2] = useState(false);
   return (
     <>
       <div className="py-5">
@@ -12,21 +14,21 @@ const Catagories = () => {
           Shop by Category
         </h3>
 
-        <div className="border-b border-b-[#d3d3d3]">
+        <div className="border-b border-b-[#d3d3d3] py-4">
           <button className={"cursor-pointer"} onClick={() => setMenu(!menu)}>
-            <div className="flex items-center justify-between w-[370px] h-[70px]">
-              <p className={"font-normal text-base text-[#767676]"}>
+            <div className="flex items-center justify-between w-[370px]">
+              <p className={"font-normal text-base text-[#767676] pb-1"}>
                 Category 1
               </p>
 
               {menu ? (
-                <HiOutlineMinusSm className={"text-[#767676]"} />
+                <FaArrowDownLong className={"text-[#767676] text-[12px]"} />
               ) : (
-                <HiOutlinePlusSm className={"text-[#767676]"} />
+                <FaArrowRightLong className={"text-[#767676] text-[12px]"} />
               )}
             </div>
           </button>
-
+          {/* ========== menu ======= */}
           <div
             className={`pl-3 pb-3 
               ${menu ? "block" : "hidden"}`}
@@ -36,11 +38,135 @@ const Catagories = () => {
               className={"cursor-pointer"}
               onClick={() => setSubmenu(!subMenu)}
             >
-              <h3 className={"font-normal text-base text-[#767676] pb-1"}>
-                Menu
-              </h3>
+              <div className="flex justify-between items-center w-[358px]">
+                <h3 className={"font-normal text-base text-[#767676] pb-1"}>
+                  Menu
+                </h3>
+                {subMenu ? (
+                  <HiOutlineMinusSm className={"text-[#767676]"} />
+                ) : (
+                  <HiOutlinePlusSm className={"text-[#767676]"} />
+                )}
+              </div>
             </button>
-            <div className={`pl-3 ${subMenu ? "block" : "hidden"}`}>
+            <div
+              className={`pl-3 
+              ${subMenu ? "block" : "hidden"}`}
+            >
+              <h3 className={"font-normal text-base text-[#767676] pb-1"}>
+                Sub menu
+              </h3>
+            </div>
+            {/* ======== sub menu end===== */}
+            <h3 className={"font-normal text-base text-[#767676] pb-1"}>
+              Menu
+            </h3>
+            <h3 className={"font-normal text-base text-[#767676] pb-1"}>
+              Menu
+            </h3>
+            <h3 className={"font-normal text-base text-[#767676] pb-1"}>
+              Menu
+            </h3>
+          </div>
+        </div>
+
+        {/* =======2======== */}
+        <div className="border-b border-b-[#d3d3d3] py-4">
+          <button className={"cursor-pointer"} onClick={() => setMenu2(!menu2)}>
+            <div className="flex items-center justify-between w-[370px]">
+              <p className={"font-normal text-base text-[#767676] pb-1"}>
+                Category 2
+              </p>
+
+              {menu2 ? (
+                <FaArrowDownLong className={"text-[#767676] text-[12px]"} />
+              ) : (
+                <FaArrowRightLong className={"text-[#767676] text-[12px]"} />
+              )}
+            </div>
+          </button>
+          {/* ========== menu ======= */}
+          <div
+            className={`pl-3 pb-3 
+              ${menu2 ? "block" : "hidden"}`}
+          >
+            {/* ======== sub menu===== */}
+            <button
+              className={"cursor-pointer"}
+              onClick={() => setSubmenu2(!subMenu2)}
+            >
+              <div className="flex justify-between items-center w-[358px]">
+                <h3 className={"font-normal text-base text-[#767676] pb-1"}>
+                  Menu
+                </h3>
+                {subMenu2 ? (
+                  <HiOutlineMinusSm className={"text-[#767676]"} />
+                ) : (
+                  <HiOutlinePlusSm className={"text-[#767676]"} />
+                )}
+              </div>
+            </button>
+            <div
+              className={`pl-3 
+              ${subMenu2 ? "block" : "hidden"}`}
+            >
+              <h3 className={"font-normal text-base text-[#767676] pb-1"}>
+                Sub menu
+              </h3>
+            </div>
+            {/* ======== sub menu end===== */}
+            <h3 className={"font-normal text-base text-[#767676] pb-1"}>
+              Menu
+            </h3>
+            <h3 className={"font-normal text-base text-[#767676] pb-1"}>
+              Menu
+            </h3>
+            <h3 className={"font-normal text-base text-[#767676] pb-1"}>
+              Menu
+            </h3>
+          </div>
+        </div>
+
+        {/* ===========3========== */}
+        <div className="border-b border-b-[#d3d3d3] py-4">
+          <button className={"cursor-pointer"} onClick={() => setMenu2(!menu2)}>
+            <div className="flex items-center justify-between w-[370px]">
+              <p className={"font-normal text-base text-[#767676] pb-1"}>
+                Category 2
+              </p>
+
+              {menu2 ? (
+                <FaArrowDownLong className={"text-[#767676] text-[12px]"} />
+              ) : (
+                <FaArrowRightLong className={"text-[#767676] text-[12px]"} />
+              )}
+            </div>
+          </button>
+          {/* ========== menu ======= */}
+          <div
+            className={`pl-3 pb-3 
+              ${menu2 ? "block" : "hidden"}`}
+          >
+            {/* ======== sub menu===== */}
+            <button
+              className={"cursor-pointer"}
+              onClick={() => setSubmenu2(!subMenu2)}
+            >
+              <div className="flex justify-between items-center w-[358px]">
+                <h3 className={"font-normal text-base text-[#767676] pb-1"}>
+                  Menu
+                </h3>
+                {subMenu2 ? (
+                  <HiOutlineMinusSm className={"text-[#767676]"} />
+                ) : (
+                  <HiOutlinePlusSm className={"text-[#767676]"} />
+                )}
+              </div>
+            </button>
+            <div
+              className={`pl-3 
+              ${subMenu2 ? "block" : "hidden"}`}
+            >
               <h3 className={"font-normal text-base text-[#767676] pb-1"}>
                 Sub menu
               </h3>
