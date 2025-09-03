4e211ada19fa04995e432f5940f718fa8db28474
motasimdev <themotasimdev@gmail.com>
2025-09-03 16:54:45 +0600

    responsive running

--- src/components/layouts/ShopContent.jsx
+++ src/components/layouts/ShopContent.jsx
@@ -24,21 +24,21 @@ const ShopContent = () => {
       <div className="">
         <Container>
           <div className="flex items-start">
-            <div className="hidden lg:w-[30%]">
+            <div className="hidden lg:block lg:w-[30%]">
               <Catagories />
               <ColorCatagry />
               <BrandCatagry />
               <PriceCatgry />
             </div>
             <div className="lg:w-[70%] lg:pt-3 pl-3">
-              <div className="flex items-center gap-4 justify-end">
+              <div className="flex items-center gap-4 justify-end pr-3 lg:pr-0">
                 <h3 className="font-normal lg:text-base text-[10px] text-[#767676]">
                   Sort by:
                 </h3>
                 <div className="relative">
                   <select
                     name="choice"
-                    className="border border-[#F0F0F0] w-[90px] lg:w-[200px] text-[#767676fd] text-[10px] px-2 lg:px-5 py-1 lg:py-2 appearance-none focus:outline-none focus:border-gray-300 cursor-pointer"
+                    className="border border-[#F0F0F0] w-[90px] lg:w-[200px] text-[#767676fd] text-[10px] lg:text-base px-2 lg:px-5 py-1 lg:py-2 appearance-none focus:outline-none focus:border-gray-300 cursor-pointer"
                   >
                     <option
                       value="Feature"
@@ -59,43 +59,43 @@ const ShopContent = () => {
                       Price High to Low
                     </option>
                   </select>
-                  <FaCaretDown className="absolute top-1/2 -translate-y-1/2 right-1 lg:right-3 pointer-events-none text-[#737373]" />
+                  <FaCaretDown className="text-[10px] lg:text-base absolute top-1/2 -translate-y-1/2 right-1 lg:right-3 pointer-events-none text-[#737373]" />
                 </div>
                 <div className="relative flex items-center gap-4">
-                  <h3 className="font-normal text-base text-[#767676]">
-                    Sort by:
+                  <h3 className="font-normal text-[10px] lg:text-base text-[#767676]">
+                    Show:
                   </h3>
                   <select
                     name="choice"
-                    className="border border-[#F0F0F0] w-[90px] pl-4 py-2 appearance-none focus:outline-none focus:border-gray-400 cursor-pointer"
+                    className="border border-[#F0F0F0] w-60px lg:w-[90px] pl-2 pr-4 lg:pl-4 py-1 lg:py-2 appearance-none focus:outline-none focus:border-gray-300 cursor-pointer lg:text-base text-[#767676] text-[10px]"
                   >
                     <option
                       value="Feature"
-                      className="font-normal text-base text-[#767676]"
+                      className="font-normal text-[10px] lg:text-base text-[#767676]"
                     >
                       36
                     </option>
                     <option
                       value="Price Low to High"
-                      className="font-normal text-base text-[#767676]"
+                      className="font-normal text-[10px] lg:text-base text-[#767676]"
                     >
                       40
                     </option>
                     <option
                       value="Price High to Low"
-                      className="font-normal text-base text-[#767676]"
+                      className="font-normal text-[10px] lg:text-base text-[#767676]"
                     >
                       50
                     </option>
                   </select>
-                  <FaCaretDown className="absolute top-1/2 -translate-y-1/2 right-3 pointer-events-none text-[#737373]" />
+                  <FaCaretDown className="text-[10px] lg:text-base absolute top-1/2 -translate-y-1/2 right-1 lg:right-3 pointer-events-none text-[#737373]" />
                 </div>
               </div>
 
               {/* ======================================== */}
-              <div className=" flex flex-wrap justify-between gap-y-3 pt-13">
+              <div className=" flex flex-wrap md:justify-between justify-around gap-y-3 pt-2 md:pt-13">
                 {myProducts.map((item) => (
-                  <div className="w-[300px]" key={item.id}>
+                  <div className="w-[150px] md:w-[300px]" key={item.id}>
                     <SingleProduct
                       imgSrc={item.thumbnail}
                       alt={item.thumbnail}
