8fc709d14c7259a7f404f65549660bd7144145ec
motasimdev <themotasimdev@gmail.com>
2025-09-03 15:46:05 +0600

    shop page responsive start

--- src/components/layouts/ShopTitle.jsx
+++ src/components/layouts/ShopTitle.jsx
@@ -6,26 +6,26 @@ import { IoIosArrowForward } from "react-icons/io";
 const ShopTitle = () => {
   return (
     <>
-      <div className="pt-[50px] pb-[30px]">
+      <div className="pt-2 lg:pt-[50px] pb-2 lg:pb-[30px] pl-3">
         <Container>
           <Heading
             text={"Products"}
             as={"p"}
-            className={"text-[49px] font-bold text-[#262626]"}
+            className={"text-[25px] lg:text-[49px] font-bold text-[#262626]"}
           />
-          <div className="flex items-center pt-5 pb-[65px]">
+          <div className="flex items-center pt-1 lg:pt-5 pb-[65px]">
             <Heading
               text={"Home"}
               as={"p"}
-              className={"text-[#767676] text-[12px] font-normal"}
+              className={"text-[#767676] text-[9px] lg:text-[12px] font-normal"}
             />
             <IoIosArrowForward
-              className={"mx-2 text-[#767676] text-[12px] font-normal"}
+              className={"mx-2 text-[#767676] text-[9px] lg:text-[12px] font-normal"}
             />
             <Heading
               text={"Shop"}
               as={"p"}
-              className={"text-[#767676] text-[12px] font-normal"}
+              className={"text-[#767676] text-[9px] lg:text-[12px] font-normal"}
             />
           </div>
         </Container>
