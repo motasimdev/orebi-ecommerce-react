8802245c0570c4e894f0d6c811c84b4639c61009
motasimdev <themotasimdev@gmail.com>
2025-09-08 10:00:05 +0600

    my Account title

---
+++ src/components/layouts/MyAccountTitle.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const MyAccountTitle = () => {
+  return (
+    <>
+    <div className="pt-2 lg:pt-[50px] pb-2 lg:pb-[30px] pl-3 lg:pl-0">
+        <Container>
+          <div className="">
+            <Heading
+              text={"My Account"}
+              as={"p"}
+              className={"text-[25px] lg:text-[49px] font-bold text-[#262626]"}
+            />
+            <div className="flex items-center pt-1 lg:pt-5 lg:pb-[65px]">
+              <Heading
+                text={"Home"}
+                as={"p"}
+                className={
+                  "text-[#767676] text-[9px] lg:text-[12px] font-normal"
+                }
+              />
+              <IoIosArrowForward
+                className={
+                  "mx-2 text-[#767676] text-[9px] lg:text-[12px] font-normal"
+                }
+              />
+              <Heading
+                text={"My Account"}
+                as={"p"}
+                className={
+                  "text-[#767676] text-[9px] lg:text-[12px] font-normal"
+                }
+              />
+            </div>
+          </div>
+        </Container>
+      </div>
+    </>
+  )
+}
+
+export default MyAccountTitle
--- src/components/pages/MyAccount.jsx
+++ src/components/pages/MyAccount.jsx
@@ -3,38 +3,7 @@ import React from "react";
 const MyAccount = () => {
   return (
     <>
-      <div className="pt-2 lg:pt-[50px] pb-2 lg:pb-[30px] pl-3 lg:pl-0">
-        <Container>
-          <div className="">
-            <Heading
-              text={"Products"}
-              as={"p"}
-              className={"text-[25px] lg:text-[49px] font-bold text-[#262626]"}
-            />
-            <div className="flex items-center pt-1 lg:pt-5 lg:pb-[65px]">
-              <Heading
-                text={"Home"}
-                as={"p"}
-                className={
-                  "text-[#767676] text-[9px] lg:text-[12px] font-normal"
-                }
-              />
-              <IoIosArrowForward
-                className={
-                  "mx-2 text-[#767676] text-[9px] lg:text-[12px] font-normal"
-                }
-              />
-              <Heading
-                text={"Shop"}
-                as={"p"}
-                className={
-                  "text-[#767676] text-[9px] lg:text-[12px] font-normal"
-                }
-              />
-            </div>
-          </div>
-        </Container>
-      </div>
+      
     </>
   );
 };
