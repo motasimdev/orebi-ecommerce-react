2122af8ef961dc0f552466f2d0201a070f58cb32
motasimdev <themotasimdev@gmail.com>
2025-10-07 06:14:32 +0600

    ok

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,36 +1,35 @@
-import React from 'react'
-import Banner from '../layouts/Banner'
-import Information from '../layouts/Information'
-import Ads from '../layouts/Ads'
+import React from "react";
+import Banner from "../layouts/Banner";
+import Information from "../layouts/Information";
+import Ads from "../layouts/Ads";
 import { motion, useScroll } from "motion/react";
-import NewArrival from '../layouts/NewArrival';
-import BestSellers from '../layouts/BestSellers';
-import AdsTwo from '../layouts/AdsTwo';
-import SpecialOffers from '../layouts/SpecialOffers';
-
+import NewArrival from "../layouts/NewArrival";
+import BestSellers from "../layouts/BestSellers";
+import AdsTwo from "../layouts/AdsTwo";
+import SpecialOffers from "../layouts/SpecialOffers";
 
 const Home = () => {
-
   const { scrollYProgress } = useScroll();
   return (
     <>
-    <motion.div 
-      className="h-2 bg-[#717171] w-full origin-left fixed top-0 left-0 z-[100]"
-      style={{
-        scaleX: scrollYProgress
-      }}
-      >
-
-      </motion.div>
-    <Banner/>
-    <Information/>
-    <Ads/>
-    <NewArrival/>
-    <BestSellers/>
-    <AdsTwo/>
-    <SpecialOffers/>
+      <motion.div
+        className="h-2 bg-[#717171] w-full origin-left fixed top-0 left-0 z-[100]"
+        style={{
+          scaleX: scrollYProgress,
+        }}
+        initial={{
+          scaleX:0,
+        }}
+      ></motion.div>
+      <Banner />
+      <Information />
+      <Ads />
+      <NewArrival />
+      <BestSellers />
+      <AdsTwo />
+      <SpecialOffers />
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
