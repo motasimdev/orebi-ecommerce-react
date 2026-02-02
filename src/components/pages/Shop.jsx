b851d37750d10279ba35d7b6b6db19b0cf7be967
MD Motasim Billah <themotasimdev@gmail.com>
2026-02-02 23:47:48 +0600

    optimize

--- src/components/pages/Shop.jsx
+++ src/components/pages/Shop.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import Motion from '../layouts/Motion'
 import ShopTitle from '../layouts/ShopTitle'
-import SingleProduct from '../SingleProduct'
 import ShopContent from '../layouts/ShopContent'
 
 const Shop = () => {
